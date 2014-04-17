772c14abb9d87c54a284b3b88ab4ba0eed593cea
shumin <shumin_92@hotmail.com>
2014-04-17 17:33:29 +0800

    layout modified

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-var t;
-var count=0;
-var count_back;
-
-var articles=new Array();
+var articles=new Array(); //array of articles
 articles[0]=new article("0","Paris restricts car use after pollution hits high","Alternative driving days are being introduced in the French capital, Paris, in an attempt to tackle dangerous levels of air pollution.<br><br>From Monday, drivers will only be able to use their vehicles every other day.<br><br>This is only the second time since 1997 that such a restriction has been enforced.<br><br>The French government made the decision after air pollution exceeded safe levels for five days running in Paris and surrounding areas.<br><br>Motorcycles will also be covered by the ban, which will allow only vehicles with number plates terminating in an uneven digit to take to the roads, beginning at 05:30 (04:30 GMT) on Monday.<br><br>On Friday, public transport was made free of charge for three days in an attempt to encourage people to leave their cars at home. This measure will continue on Monday.<br><br>The smoggy conditions have been caused by a combination of cold nights and warm days, which have prevented pollution from dispersing.<br><br>The capital's air quality has been one of the worst on record, French environmental agencies say, rivalling the Chinese capital, Beijing, one of the world's most polluted cities.<br><br>On Friday, pollution levels hit 180 microgrammes of PM10 particulates per cubic metre, more than double the safe limit of 80.<br><br>PM10 particulates are emitted by vehicles, heating systems and heavy industry.<br><br>The government will review pollution levels on Monday, before deciding whether to extend the driving restrictions for longer.");
 articles[1]=new article("1","US 'disturbed' by death of Chinese activist Cao Shunli","The United States has said it is \"deeply disturbed\" by reports of the death of activist Cao Shunli at a hospital in Beijing.<br><br>Cao's lawyers said she had died in detention after being denied medical treatment for several months.<br><br>The human rights activist was arrested at Beijing airport in September, when she attempted to board a flight to Geneva to attend a UN training course.<br><br>She had previously served at least two terms in prison camps for her activism.<br><br>Human Rights Watch said that Cao was only transferred to hospital in February, despite her medical condition worsening in her months in detention.<br><br>Cao had advocated for the right of independent civilians to participate in the Chinese government's drafting of human rights reports.<br><br>US State Department spokeswoman Jen Psaki said that the US had repeatedly raised concerns about Cao Shunli's detention and deteriorating health with the Chinese authorities.");
 articles[2]=new article("2","Europe backs call for universal smartphone charger","The maddening scramble to find the right charger for your smartphone could be coming to an end.<br><br>European politicians have backed a regulation that will force smartphone makers to use just one type of charger.<br><br>On Thursday MEPs voted overwhelmingly in favour of the regulation, which will see a common charger used for smartphones by 2017.<br><br>The regulation was drawn up to help consumers and to cut down on the amount of electronic waste.<br><br>\"This serves the interests both of consumers and the environment,\" said MEP Barbara Weiler in a statement. \"It will put an end to charger clutter and 51,000 tonnes of electronic waste annually.\"<br><br>The regulation is still only a draft law and must be approved by Europe's council of ministers. However, that body has already given its informal backing to the law, suggesting it will win final approval.<br><br>European member states will have until 2016 to translate the regulation into national laws and manufacturers will then have 12 months to switch to the new design. The process of getting manufacturers to agree on a common design began in 2009.<br><br>The design of charger being favoured uses a Micro USB connector - a format used on many handsets and other devices already.<br><br>Apple has yet to respond to the proposals as it is one of the few manufacturers that produces gadgets with proprietary power ports. However, it was one of the original signatories of the agreement Europe reached with manufacturers to produce the chargers.");
@@ -13,80 +9,76 @@ articles[6]=new article("6","China knife attack in Hunan's Changsha leaves six d
 articles[7]=new article("7","About World Peace","Students at William Dandy Middle School, a magnet school in Fort Lauderdale that attracts students from throughout the county, tell what they would do to bring world peace.<br><br>\"To bring peace into this world, it would take a lot of strength. The reason for that is because a lot of people don't respect each other. I think if everyone in this world respected each other and each other's rights, the world would be better.\"<br>-Phillipe Sydney<br><br>\"I know in order to bring world peace, I have to first start off by loving myself. And then show my love to the world. And hopefully by me doing so, some others could love themselves and in return give their love to the world. ... With all the lethal weapons America has and keeps making, we might never have world peace. But if everyone tries their hardest, we, the people, can do it.\"<br>-Elijah Bell<br><br>\"What can I do to bring peace to the world? A big question to answer for many people. But I can answer it in just three words: `Stop the hate!' Hate is what is causing the violence. If we stop hating each other and learn to respect our difference, maybe the violence can stop.\"<br>-Myrmose Marcellon<br><br>\"I can bring world peace by not fighting, and helping other people.\"<br>-Sinora Hayes<br><br>\"To bring world peace, I could have a meeting just for the people that don't get along. I could also have a meeting for those people who do get along and tell them to help me out.\"<br>-Khalilah Adams<br><br>\"The things we can do to bring world peace are: we can have world peace talks; we can stop interfering with other countries' wars; bar firearms so people can't get robbed; we can cut off all TV shows that are violent, like Beavis and Butt-Head.\"<br>-Timmy Giang");
 articles[8]=new article("8","Remember our veterans today, everyday","I share my respect to all veterans. I am heartfelt to our World War ll veterans. They are our elder family members who fought for our country, other countries and for world peace.<br><br>As a Vietnam veteran, I remember the day I volunteered, not only to serve our country in the Army, but to help in Vietnam. Veterans are giving to the community, as are their families. I believe we have to give differently. We have to share that wars have to be averted, and agreements reached that will be fair and protective for all. Yes, we have to communicate more. There is disparity.<br><br>There are more civilians killed in a war than the individuals who are trained and wear the equipment appropriate for combat. Please, in this age of technology and Internet, go to Wikipedia and learn that in Iraq and Afghanistan there were thousands of civilians killed. In Korea, during a three-year period, there were three million. In Vietnam, some hundreds of thousands were killed, and in World War ll, more than 40 million.<br><br>These are low estimates. Of course, we do not want the same in the United States. This is a day to think about all people. I believe it is time for the veterans to speak up for world peace.");
 articles[9]=new article("9","The World Is Changing Around You So Cherish Your Moments","Everyone loves to celebrate the good moments in their lives. Whenever weddings, birthdays, anniversaries, and new babies come along people throw themselves into the happy celebrations of each moment.<br><br>However, sometimes people do not stop to realize just how fortunate they are to have these moments full of hope and happiness. Additionally, when you are faced with challenges in life you may have some trouble seeing the positive side of these experiences.<br><br>To really enjoy a happy life you need to learn how to embrace and cherish your moments. When something good comes in your life you need to stop and really think about it. Take some time to appreciate everything that happened to lead up to that moment.<br><br>Chances are there are trials and tribulations that you've had to face to get to the celebration. For example, anniversaries come from relationships that have weathered the test of time. To get to these happy events you have to hold the hope that you are going to get past any bumps in the road. And the truth is that not all of those bumps in the road are huge and devastating, but that doesn’t mean that they are any less important.<br><br>Each of those small experiences contribute to who you are as a person, and that’s why you should embrace them. That doesn’t mean that cherishing the more difficult moments in your life is not difficult. In reality we would all prefer to be happy all of the time.<br><br>Unfortunately we do not live in a perfect world and that means that you are going to face significant challenges in the course of your lifetime. While these are not happy times they are perhaps some of the most important moments that you will face. Learning how to negotiate these choppy waters successfully can be a skill that is hard earned.<br><br>One of the better ways to learn to cherish your moments is to really live through the experience. While you are navigating difficult circumstances try to remind yourself that you have been given the gift of an experience that you can learn from. Keep the hope close in your heart that you are going to be successful in the long run.<br><br>A lot of times you need that hope to help bolster your self confidence so that you can come out of negative circumstances in a more happy place. Again, it is essential to remember that even though this type of complication in your life can be difficult you should do what you can to embrace it as the best thing that you could be going through at this period of your life.<br><br>Life is short! It can be over in a blink of an eye, and you may find yourself regretting that you did not embrace your experiences when you had the chance. Don’t let that happen when all you have to do is hold onto the happy moments and learn from the tough moments to create a fuller life for yourself and those around you. Cherish your moments!");
-
-var command_back;
-last_number=100;
 function article(id,title,content)
 {
 	this.id=id;
 	this.title=title;
 	this.content=content;
 }
-
-function doTimer()
+var number;
+var wrong=0;
+var point=0;
+var count=0;
+var stay=1;
+function show()
 {
-	if(!timer_on)
-	{
-		timer_on=1;
-		change_article();
-	}
+    number=Math.floor(Math.random()*9)+0;
+    var x=document.getElementById("contents").rows[0].cells;
+    x[0].innerHTML=articles[number].content;
+    
+   // document.getElementById("titles").innerHTML=articles[number].title;
+    //document.getElementById("contents").innerHTML=articles[number].content;
 }
-
-function stopTimer()
+function deleting(event)
 {
-	clearTimeout(t);
-	timer_on=0;
+	var word=event.keyCode;
+	var wordd=String.fromCharCode(word);
+	if(word==8)//backspace
+    {
+        document.getElementById("command").innerHTML="backspace";
+        character=(articles[number].content)[point];
+        checking(character,wordd);
+        document.getElementById("checkingTitle").innerHTML=character;
+        document.getElementById("checkingBody").innerHTML=wordd;
+        point--;
+    
+    }
 }
-
-function change_article()
+function gaming(event)
 {
-	number=Math.floor((Math.random()*9)+0);
-	while(number==last_number)
-	{
-		last_number=number;
-		number=Math.floor((Math.random()*9)+0);
-	}
-	for(i=0;i<9;i++)
-	{
-		if(number==articles[i].id)
-		{
-			show=articles[i];
-			break;
-		}
-	}
-	title_back=document.getElementById("titles");
-	title_back.innerHTML=show.title;
+	var word=event.keyCode;
+   	var wordd=String.fromCharCode(word);
 
-	content_back=document.getElementById("contents");
-	content_back.innerHTML=show.content;
-	
-	last_number=number;
-	
-	t=setTimeout(function(){change_article()},5000);
-	
-	count++;
-	
-	count_back=document.getElementById("count");
-	count_back.innerHTML=count;
+    
+    if(word==32)//space
+    {
+ 
+        if(wrong==0)
+            count++;
+        document.getElementById("command").innerHTML=count;
+        wrong=0;
+        point++;
+    }
+    else if(word==16)
+    {
+       document.getElementById("command").innerHTML="16";
+        stay=0;
+    }
+    else
+    {
+ //       document.getElementById("command").innerHTML=word;
+        character=(articles[number].content)[point];
+        checking(character,wordd);
+        document.getElementById("checkingTitle").innerHTML=character;
+        document.getElementById("checkingBody").innerHTML=wordd;
+        point++;
+    }
 }
-
-function stop_and_start()
+function checking(base,input)
 {
-	var x=document.getElementById("insert");
-
-	if(x.value=="stop")
-	{
-		command_back=document.getElementById("command");
-		command_back.innerHTML="STOP changing articles";
-		stopTimer();
-		
-	}
-	else if(x.value=="start")
-	{
-		command_back=document.getElementById("command");
-		command_back.innerHTML="Start changing articles";
-		doTimer();
-	}
+    if(base!=input)
+    {
+        wrong++;
+    }
 }
-
-change_article();
+show();
